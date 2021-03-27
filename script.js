5648b2a4de76d86bc8b4d06b72b9fcd2bdf5c94c
Lidor Eliyahu Shelef <ivanlidor98@gmail.com>
2021-03-28 02:27:09 +0300

    Online courses
    Summary:
       - fixed some error with the rubberBand effect
       - added the online courses section and implementer the lists of the courses.

--- script.js
+++ script.js
@@ -67,10 +67,79 @@ $(document).ready(function(){
     $(".band-hover").bind("webkitAnimationEnd mozAnimationEnd animationEnd", function(){
       $(this).removeClass("rubberBand")  
     })
-
     $(".band-hover").hover(function(){
       $(this).addClass("rubberBand");        
     })
+    
+    
+    
+    
+    
+    
+//    var coll = document.getElementsByClassName("collapsible");
+    var cals = document.getElementsByClassName("card-collapsible");
+    var cons = document.getElementsByClassName("cc-content");
+    for (var i = 0; i < cals.length; i++) {
+      cals[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content, other1, other2, other3;
+        if(this.id === "0"){
+            content = cons[0];
+            other1 = cons[1];
+            other2 = cons[2];
+            other3 = cons[3];
+        }else if(this.id === "1"){
+            content = cons[1];
+            other1 = cons[0];
+            other2 = cons[2];
+            other3 = cons[3];
+        }else if(this.id === "2"){
+            content = cons[2];
+            other1 = cons[0];
+            other2 = cons[1];
+            other3 = cons[3];
+        }else if(this.id === "3"){
+            content = cons[3];
+            other1 = cons[0];
+            other2 = cons[1];
+            other3 = cons[2];
+        }
+        if (content.style.maxHeight){
+          content.style.maxHeight = null;
+          content.style.opacity = null;
+          // content.style.display = "none";
+        } else {
+          other1.style.maxHeight = null;
+          other2.style.maxHeight = null;
+          other3.style.maxHeight = null;
+          other1.style.opacity = null;
+          other2.style.opacity = null;
+          other3.style.opacity = null;
+          // other1.style.display = "none";
+          // other2.style.display = "none";
+          // content.style.display = "block";
+          content.style.maxHeight = content.scrollHeight + "px";
+          content.style.opacity = 1;
+        } 
+      });
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 });
 
